69b48c8bcb1d00ee2f9735f61fbe1abbeebd1cd3
Benny V <bengv2303@gmail.com>
2018-11-06 21:52:32 -0700

    updated index

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,157 +1,105 @@
-var crystalOne;
-var crystalTwo;
-var crystalThree;
-var crystalFour;
+$(document).ready(function () {
+
+function startGame() {
 
 
-var numberArray = [];
+    var computerRandom = Math.floor(Math.random() * (120 - 19)) + 19;
+    var button1 = Math.floor(Math.random() * (13 - 1)) + 1;
+    var button2 = Math.floor(Math.random() * (13 - 1)) + 1;
+    var button3 = Math.floor(Math.random() * (13 - 1)) + 1;
+    var button4 = Math.floor(Math.random() * (13 - 1)) + 1;
+    var userTotal = 0;
+    var losses = 0;
+    var wins = 0;
 
-for (var i = 1; i <= 120; i++) {
-    numberArray.push(i);
-}
-
-console.log(numberArray);
-
-var computerRandom;
-var button1;
-var button2;
-var button3;
-var button4;
-var userTotal = 0;
-
-var score;
-
-var wins;
-var losses;
-
-// if (userRandomNumber.toNumber() === computerRandomNumber.toNumber()) {
-//     // Add to the win counter
-//     wins++;
-
-//     // Give the user an alert
-//     alert("You win!");
-
-//     // Update the win counter in the HTML
-//     document.getElementById("wins").innerHTML = wins;
-
-//     // Restart the game
-//     startGame();
-// }
-
-//     // If we've run out of guesses
-//     else if (numGuesses === 0) {
-
-//         // Add to the loss counter
-//         losses++;
-
-//         // Give the user an alert
-//         alert("You lose");
-
-//         // Update the loss counter in the HTML
-//         document.getElementById("losses").innerHTML = losses;
-
-        // Restart the game
-        // startGame();
-
-    // }
-
-    //generate randomNumber
-
-    // generate randomNumber for each crystal
-
-    // need a click event when a crystal is clicked
-    //capture the value of the crystal when it's clicked 
-    // add points to players total score
-    // if/else if statement
-    //if score is = randomNumber then the user wins
-    //else if score is > randomNumber then the user loses
-    // wins ++
-    // losses ++
-    // generate all random numbers again
-
-    // startGame()
-    // It's how we we will start and restart the game.
-    // (Note: It's not being run here. Function declarations like this are made for future use.)
-    // function startGame() {
-
-    //     score = 0;
-    //     numGuesses = [];
-    //     numberPicked = [];
-    // }
-    // console.log(numberPicked);
+    $("#button-1").attr("crystalValue", button1);
+    $("#button-2").attr("crystalValue", button2);
+    $("#button-3").attr("crystalValue", button3);
+    $("#button-4").attr("crystalValue", button4);
+    $(".random-number").text(computerRandom);
+    
 
 
-    // roundComplete() function
-    // Here we will have all of the code that needs to be run after each guess is made.
-    // function roundComplete() {
+    // var lockGame = false;
 
-    //     // First, log an initial status update in the console
-    //     // telling us how many wins, losses, and guesses are left.
-    //     console.log("wins: " + wins + " | losses: " + losses + " | numGuesses: " + numGuesses);
+    console.log(computerRandom);
 
-    //     // HTML UPDATES
-    //     // ============
 
-    //     // Update the HTML to reflect the new number of guesses.
-    //     document.getElementById("guesses-left").innerHTML = numGuesses;
 
-    //     // This will print the array of guesses and blanks onto the page.
-    //     document.getElementById("word-blanks").innerHTML = blanksAndSuccesses.join(" ");
+    $(".button").on("click", function () {
+        // userTotal = userTotal + +button1;
+        // console.log($(this));
+        console.log("Computer Pick: " + computerRandom);
+        // console.log("button 1 = " + button1);
+        // console.log("userTotal =  " + userTotal);
 
-    //     // This will print the wrong guesses onto the page.
-    //     document.getElementById("wrong-guesses").innerHTML = wrongGuesses.join(" ");
+        var crystalValue = $(this).attr("crystalValue");
+        console.log("crystalValue" + crystalValue);
 
-    //     // If our Word Guess string equals the solution.
-    //     // (meaning that we guessed all the letters to match the solution)...
-    //     if (lettersInChosenWord.toString() === blanksAndSuccesses.toString()) {
+        userTotal = userTotal + parseInt(crystalValue);
+        console.log("userTotal = " + userTotal);
 
-    //         // Add to the win counter
-    //         winCounter++;
+        if (userTotal > computerRandom) {
+            losses++;
+            $("#result").text("Busted!");
 
-    //         // Give the user an alert
-    //         alert("You win!");
+            startGame();
 
-    //         // Update the win counter in the HTML
-    //         document.getElementById("win-counter").innerHTML = winCounter;
+        } else if (userTotal === computerRandom) {
+            wins++;
 
-    //         // Restart the game
-    //         startGame();
-    //     }
+            $("#result").text("Winner!");
 
-    //     // If we've run out of guesses
-    //     else if (numGuesses === 0) {
+            startGame();
 
-    //         // Add to the loss counter
-    //         lossCounter++;
+        } else {
 
-    //         // Give the user an alert
-    //         alert("You lose");
+            $("#result").text("Keep Guessing!");
+        } 
+        
+        $(".userTotal").text(userTotal);
+    });
 
-    //         // Update the loss counter in the HTML
-    //         document.getElementById("loss-counter").innerHTML = lossCounter;
 
-    //         // Restart the game
-    //         startGame();
 
-    //     }
+    // <!-- Couldn't get these to run  -->
 
-    // }
+    // document.getElementById("#button-1").value = "userTotal";
 
-    // // MAIN PROCESS (THIS IS THE CODE THAT CONTROLS WHAT IS ACTUALLY RUN)
-    // // ==================================================================
+    // document.getElementById("#button-2").value = "userTotal";
 
-    // // Starts the Game by running the startGame() function
-    // startGame();
+    // document.getElementById("#button-3").value = "userTotal";
 
-    // // Then initiates the function for capturing key clicks.
-    // document.onkeyup = function (event) {
+    // document.getElementById("#button-4").value = "userTotal";
 
-    //     // Converts all key clicks to lowercase letters.
-    //     letterGuessed = String.fromCharCode(event.which).toLowerCase();
+    // document.getElementById("userTotal").value = "userTotal";
+    // ------------------------------------------------------------------------------------------------------------
+    // document.getElementById("#button-1").addEventListener("click", myFunction);
 
-    //     // Runs the code to check for correct guesses.
-    //     checkLetters(letterGuessed);
-
-    //     // Runs the code that ends each round.
-    //     roundComplete();
+    // function myFunction() {
+    //   document.getElementById("userTotal").innerHTML = "userTotal";
     // };
+
+    // document.getElementById("#button-2").addEventListener("click", myFunction);
+
+    // function myFunction() {
+    //   document.getElementById("userTotal").innerHTML = "userTotal";
+    // };
+
+    // document.getElementById("#button-3").addEventListener("click", myFunction);
+
+    // function myFunction() {
+    //   document.getElementById("userTotal").innerHTML = "userTotal";
+    // };
+
+    // document.getElementById("#button-4").addEventListener("click", myFunction);
+
+    // function myFunction() {
+    //   document.getElementById("userTotal").innerHTML = "userTotal";
+    // };
+
+    // Could not figure out the correct If/else statement to get the right logic.
+
+}
+startGame();
+});
